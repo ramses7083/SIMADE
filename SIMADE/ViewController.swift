7ad78347553e0849637d50750a8d9b4761364296
Ramses Miramontes Meza <Ramses7083@MacBook-Pro-de-Ramses.local>
2015-09-01 16:32:36 -0500

    Initial Commit

---
+++ SIMADE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIMADE
+//
+//  Created by Ramses Miramontes Meza on 01/09/15.
+//  Copyright (c) 2015 Ramses Miramontes Meza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
